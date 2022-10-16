0dc66cb9c70f18baaddec0e87c4238b744f77d78
hary <2339105817@qq.com>
2022-10-16 13:06:45 +0800

    feat:1

--- src/module-hmmm/pages/questions-choice.vue
+++ src/module-hmmm/pages/questions-choice.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class='container'>精选题库</div>
+  <div class="container">
+    <el-card class="box-card">
+      <el-button type="success " icon="el-icon-edit" class="add"
+        >新增学科</el-button
+      >
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.container {
+  .add {
+    float: right;
+  }
+}
+</style>
+
